866f2d8af3c9bffde53df676d8670890e24df277
Rene Geevers <rene.geevers@sanoma.com>
2026-09-08 19:27:44 +0200

    chore: remove build output from the branch

--- dist/accessories/EnergyCharacteristics.d.ts
+++
@@ -1,4 +0,0 @@
-import type { EnergyCharacteristicsMap } from '../types';
-declare const createEnergyCharacteristics: (Characteristic: any) => EnergyCharacteristicsMap;
-export default createEnergyCharacteristics;
-//# sourceMappingURL=EnergyCharacteristics.d.ts.map
